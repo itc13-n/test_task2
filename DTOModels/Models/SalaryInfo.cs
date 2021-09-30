482d098543aec50b70cdb67b537293a9fe82dcef
Mark.n.v.1113 <Mark.n.v.1113@gmail.com>
2021-09-30 23:08:41 +0300

    SalaryInfo added (as Salaries table DTO)

---
+++ DTOModels/Models/SalaryInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOModels.Models
+{
+    public class SalaryInfo
+    {
+        public int ID { get; set; }
+        public int Level { get; set; }
+        public double BonusYear { get; set; }
+        public double BonusSubordinates { get; set; }
+    }
+}
